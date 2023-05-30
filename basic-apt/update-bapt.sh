fd31886d25b0e3e46689757e1070e56a4f26a7f4
josefhofer <me@jhofer.de>
2023-05-30 11:38:26 +0200

    added basic update-bapt.sh (basic apt) modified pf (echos)

--- basic-apt/update-bapt.sh
+++ basic-apt/update-bapt.sh
@@ -1,8 +1,18 @@
-echo
-echo -e "[INFO] GETTING UPDATES \n"
+#!/bin/bash
+
+print_info() {
+  echo "[INFO] $1"
+  echo
+}
+
+print_info "GETTING UPDATES"
+
 sudo apt update
 echo
-echo -e "[INFO] YEAH UPGRADE \n"
+
+print_info "YEAH UPGRADE"
+
 sudo apt upgrade -y
 echo
-echo -e "[INFO] FINISHED UPDATING! \n"
+
+print_info "FINISHED UPDATING!"
--- pfsense/update-pf.sh
+++ pfsense/update-pf.sh
@@ -1,31 +1,32 @@
 #!/bin/sh
 
-# Update package repositories
-echo
-echo -e "[INFO] GETTING UPDATES \n"
+print_info() {
+  echo "[INFO] $1"
+  echo
+}
+
+print_info "GETTING UPDATES"
+
 pkg update
-
-# Upgrade installed packages
 echo
-echo -e "[INFO] YEAH UPGRADE \n"
-pkg upgrade -y
 
-# Check for pfSense updates
+print_info "YEAH UPGRADE"
+
+pkg upgrade -y
+echo
+
+print_info "CHECKING FOR PFSENSE UPDATES"
+
 pfsense_version=$(cat /etc/version)
 latest_version=$(fetch -qo - https://updates.pfsense.org/_updaters/amd64/version)
 echo
-echo "Current pfSense version: $pfsense_version"
-echo "Latest pfSense version: $latest_version"
+print_info "Current pfSense version: $pfsense_version, Latest pfSense version: $latest_version"
 
 if [ "$latest_version" != "$pfsense_version" ]; then
-    echo
-    echo "A pfSense update is available. Performing update..."
-    pkg upgrade -r pfSense -y
-    echo
-    echo -e "[INFO] FINISHED UPDATING! \n"
-    echo "pfSense update completed."
+  print_info "A pfSense update is available. Performing update..."
+  pkg upgrade -r pfSense -y
+  echo
+  print_info "FINISHED UPDATING! pfSense update completed"
 else
-    echo
-    echo -e "[INFO] FINISHED UPDATING! \n"
-    echo "No pfSense update available."
+  print_info "FINISHED UPDATING! No pfSense update available"
 fi
