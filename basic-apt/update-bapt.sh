24df3fc9c1b2fb2e14211527d1f42b06dca1bde2
josefhofer <me@jhofer.de>
2023-05-30 12:04:02 +0200

    updated update-bapt.sh, update-pf.sh

--- basic-apt/update-bapt.sh
+++ basic-apt/update-bapt.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
-print_info() {
-  echo "[INFO] $1"
+# Function for logging
+log_info() {
+  local log_message="$1"
+  echo
+  echo "[INFO] ${log_message}"
   echo
 }
 
-print_info "GETTING UPDATES"
+# Function for updating package list
+update_package_list() {
+  log_info "Updating package list"
+  if ! sudo apt update; then
+    log_info "Failed to update package list"
+    exit 1
+  fi
+}
 
-sudo apt update
-echo
+# Function for upgrading installed packages
+upgrade_packages() {
+  log_info "Upgrading installed packages"
+  if ! sudo apt upgrade -y; then
+    log_info "Failed to upgrade installed packages"
+    exit 1
+  fi
+}
 
-print_info "YEAH UPGRADE"
-
-sudo apt upgrade -y
-echo
-
-print_info "FINISHED UPDATING!"
+# Execution
+update_package_list
+upgrade_packages
+log_info "Script execution finished successfully"
--- pfsense/update-pf.sh
+++ pfsense/update-pf.sh
@@ -1,32 +1,64 @@
 #!/bin/sh
 
-print_info() {
-  echo "[INFO] $1"
+# Function for logging
+log_info() {
+  log_message="$1"
+  echo
+  echo "[INFO] ${log_message}"
   echo
 }
 
-print_info "GETTING UPDATES"
+# Function for updating package repository
+update_package_repo() {
+  log_info "Updating package repository"
+  if ! pkg update; then
+    log_info "Failed to update package repository"
+    exit 1
+  fi
+}
 
-pkg update
-echo
+# Function for upgrading installed packages
+upgrade_packages() {
+  log_info "Upgrading installed packages"
+  if ! pkg upgrade -y; then
+    log_info "Failed to upgrade installed packages"
+    exit 1
+  fi
+}
 
-print_info "YEAH UPGRADE"
+# Function for checking and updating pfSense
+check_update_pfsense() {
+  log_info "Checking for pfSense updates"
 
-pkg upgrade -y
-echo
+  pfsense_version
+  latest_version
 
-print_info "CHECKING FOR PFSENSE UPDATES"
+  if ! pfsense_version=$(cat /etc/version); then
+    log_info "Failed to read pfSense version"
+    exit 1
+  fi
 
-pfsense_version=$(cat /etc/version)
-latest_version=$(fetch -qo - https://updates.pfsense.org/_updaters/amd64/version)
-echo
-print_info "Current pfSense version: $pfsense_version, Latest pfSense version: $latest_version"
+  if ! latest_version=$(fetch -qo - https://updates.pfsense.org/_updaters/amd64/version); then
+    log_info "Failed to fetch latest pfSense version"
+    exit 1
+  fi
 
-if [ "$latest_version" != "$pfsense_version" ]; then
-  print_info "A pfSense update is available. Performing update..."
-  pkg upgrade -r pfSense -y
-  echo
-  print_info "FINISHED UPDATING! pfSense update completed"
-else
-  print_info "FINISHED UPDATING! No pfSense update available"
-fi
+  log_info "Current pfSense version: ${pfsense_version}, Latest pfSense version: ${latest_version}"
+
+  if [ "${latest_version}" != "${pfsense_version}" ]; then
+    log_info "A pfSense update is available. Performing update..."
+    if ! pkg upgrade -r pfSense -y; then
+      log_info "Failed to update pfSense"
+      exit 1
+    fi
+    log_info "pfSense update completed successfully"
+  else
+    log_info "No pfSense update available"
+  fi
+}
+
+# Execution
+update_package_repo
+upgrade_packages
+check_update_pfsense
+log_info "Script execution finished successfully"
