ecd4867909b55cba818424d5023a94dc14cf6ab3
Josef <me@jhofer.de>
2023-05-30 15:16:54 +0200

    basic apt updates (#1)
    * added dev version for pfsense updates (untested)
    
    * added basic update-bapt.sh (basic apt)
    modified pf (echos)
    
    * added basic update-bapt.sh (basic apt)
    modified pf (echos)
    
    * updated update-bapt.sh, update-pf.sh
    
    * Create ReadME.md
    
    * removed pfsense update, basically useless because of auto update in gui
    
    * simple is better
    
    * Remove sudo (apt script) (#2)
    
    * simple is better
    
    * remove sudo from apt script
    
    * apt-get instead of apt

---
+++ basic-apt/update-bapt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Get the current date and time
+now=$(date +"%m_%d_%Y")
+
+# Define the output file name
+logfile="/var/log/update_script_$now.log"
+
+# Perform the update/upgrade and output the result to the logfile
+echo "Starting system update..." | tee -a $logfile
+apt-get update -y | tee -a $logfile
+apt-get upgrade -y | tee -a $logfile
+echo "System update completed successfully!" | tee -a $logfile
